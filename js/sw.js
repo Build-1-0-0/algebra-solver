107dfeb035e22c9f467239e8bfc01450e41a41d5
Build-1-0-0 <africancontent807@gmail.com>
2025-04-30 13:52:16 +0300

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,26 @@
+// sw.js
+const CACHE_NAME = 'algebra-solver-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/output.css',
+  '/css/styles.css',
+  '/js/main.js',
+  '/js/solver.js',
+  'https://cdn.jsdelivr.net/pyodide/v0.26.2/full/pyodide.js',
+  'https://cdn.jsdelivr.net/pyodide/v0.26.2/full/pyodide.asm.wasm',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
